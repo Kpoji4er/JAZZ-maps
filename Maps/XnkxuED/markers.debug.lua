5afceee8987b682d8b95456955c5af2968d499aa
Doc_Leevsy <166147526+Doctorleevsy@users.noreply.github.com>
2026-01-31 22:49:06 +0300

    Новая локация L6U, просьба дойти до нее и протестировать

---
+++ Maps/XnkxuED/markers.debug.lua
@@ -0,0 +1,46 @@
+{
+	{
+		Groups = false,
+		LootTableIds = {
+			"SmallContainer_Exceptional",
+		},
+		handle = 194297856,
+		map = "XnkxuED",
+		name = 'InventoryItemSpawn#856 "КОНТЕЙНЕР"',
+		path = "InventoryItemSpawn ",
+		type = "InventoryItemSpawn",
+	},
+	{
+		Groups = false,
+		LootTableIds = {
+			"LargeContainer_Exceptional",
+		},
+		handle = 269795328,
+		map = "XnkxuED",
+		name = 'InventoryItemSpawn#328 "СУНДУК"',
+		path = "InventoryItemSpawn ",
+		type = "InventoryItemSpawn",
+	},
+	{
+		Groups = false,
+		LootTableIds = {
+			"SmallContainer_Exceptional",
+		},
+		handle = 461938688,
+		map = "XnkxuED",
+		name = 'InventoryItemSpawn#688 "КОНТЕЙНЕР"',
+		path = "InventoryItemSpawn ",
+		type = "InventoryItemSpawn",
+	},
+	{
+		Groups = false,
+		LootTableIds = {
+			"SmallContainer",
+		},
+		handle = 552493056,
+		map = "XnkxuED",
+		name = 'InventoryItemSpawn#056 "СУНДУК"',
+		path = "InventoryItemSpawn ",
+		type = "InventoryItemSpawn",
+	},
+}
