7e972a54d0e578b7847c52cb0b43a3186a7ed5e6
Kpoji4er <kpojiomen@gmail.com>
2026-03-30 02:49:47 +0300

    fix

--- metadata.lua
+++ metadata.lua
@@ -7,7 +7,7 @@ return PlaceObj('ModDef', {
 	'author', "Doctor_Leevsy",
 	'version_major', 1,
 	'version_minor', 6,
-	'version', 5093,
+	'version', 5102,
 	'lua_revision', 233360,
 	'saved_with_revision', 366685,
 	'code', {
@@ -22,7 +22,7 @@ return PlaceObj('ModDef', {
 	},
 	'default_options', {},
 	'has_data', true,
-	'saved', 1774120967,
+	'saved', 1774828155,
 	'code_hash', 8311704044312267199,
 	'affected_resources', {
 		PlaceObj('ModResourcePreset', {
